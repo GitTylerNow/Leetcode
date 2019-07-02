266c2af1edc2dc5a59cc869cb5e5b15adb6807eb
GitTylerNow <44731185+GitTylerNow@users.noreply.github.com>
2019-07-02 14:46:17 -0700

    create main.java

---
+++ main.java
@@ -0,0 +1,21 @@
+import java.util.*; 
+import java.io.*;
+
+class Main {  
+  public static int FirstFactorial(int num) { 
+  
+    //Recursive. Verify the 0 factorial, otherwise return num*num-1
+    if(num==0)
+        return 1;
+    else 
+        return(num*FirstFactorial(num-1));
+    
+  } 
+  
+  public static void main (String[] args) {  
+    // keep this function call here     
+    Scanner s = new Scanner(System.in);
+    System.out.print(FirstFactorial(s.nextLine())); 
+  }   
+  
+}
